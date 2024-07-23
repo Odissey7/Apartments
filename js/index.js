33452db0a77937e16575ec494312fcdde54c2260
Odissey7 <agapidu@inbox.ru>
2024-07-23 16:28:46 +0300

    fin1

--- js/index.js
+++ js/index.js
@@ -270,3 +270,59 @@ document.querySelector('.phone_pop_1').onclick = function () {
     document.querySelector('.popup_input_text_sec_1').classList.add('popup_input_text_active');
 }
 
+
+
+
+// Получаем элементы слайдера
+const slider = document.querySelector('.slider');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+const slides = Array.from(slider.querySelectorAll('img'));
+const slideCount = slides.length;
+let slideIndex = 0;
+
+// Устанавливаем обработчики событий для кнопок
+prevButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
+
+// Функция для показа предыдущего слайда
+function showPreviousSlide() {
+slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+updateSlider();
+}
+
+// Функция для показа следующего слайда
+function showNextSlide() {
+slideIndex = (slideIndex + 1) % slideCount;
+updateSlider();
+}
+
+// Функция для обновления отображения слайдера
+// function updateSlider() {
+// slides.forEach((slide, index) => {
+//     if (index === slideIndex) {
+//     slide.style.display = 'block';
+//     } else {
+//     slide.style.display = 'none';
+//     }
+// });
+// }
+
+function updateSlider() {
+slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+    slide.classList.remove('slider_img_hidden');
+    } else {
+        slide.classList.add('slider_img_hidden');
+    }
+});
+}
+
+// Инициализация слайдера
+updateSlider();
+
+
+
+
+
+
